c9ae782c7b391f550ed76bc7ce932d5c2e9b8e46
JBBart <schwarzbartj@gmail.com>
2017-09-11 20:54:03 +0200

    - added SpreadScrape from CBS.com - added HTML radio buttons

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 from flask import Flask, request, render_template
+import smtplib # for mail
+import requests
+import itertools # for zip iteration
+import datetime # to run on pythonanywhere.com only on Thursdays
+from bs4 import BeautifulSoup
 
 
 
@@ -6,8 +11,9 @@ app = Flask(__name__) # determine the route path
 
 # @ signifies a decorator - way to wrap a function and modifying its behavior
 @app.route("/") # Homepage of the website
-def index():
-    return "Spread Bets"
+@app.route("/<user>") # If user is logged in, he is directed to his account, otherwise to regular homepage
+def index(user=None):
+    return render_template("user.html", user=user)
 
 
 @app.route("/profile/<username>") # variables are in '< >'
@@ -18,6 +24,41 @@ def profile(username):
 def week(week):
     return "This is week {}".format(week)
 
+@app.route("/spreads")
+def spread():
+    return render_template("spreads.html", hometeam = hometeam, awayteam = awayteam, spreadlist = spreadlist, length = length)
+
+
+
+
+# Spread Scrape:
+url = "https://www.cbssports.com/nfl/features/writers/expert/picks"
+r = requests.get(url)
+r_html = r.content
+soup = BeautifulSoup(r_html, "html.parser")
+
+td = soup.find_all("td")
+
+away_teams = soup.find_all("span", {"class": "teamAbbrPreview fright"})
+home_teams = soup.find_all("span", {"class": "teamAbbrPreview fleft"})
+spreads = soup.find_all("span", {"id": "lineNumber"})
+
+slist = []
+hometeam = []
+awayteam = []
+spreadlist = []
+# zip items from the three lists together
+for away, home, spread in zip(away_teams, home_teams,
+                              spreads):
+    slist.append('{} {} {} {}'.format(away.text, "@", home.text, spread.text))
+    hometeam.append(home.text)
+    awayteam.append(away.text)
+    spreadlist.append(spread.text)
+
+#slist = ("\n".join(slist))
+print(slist)
+print(spreadlist)
+length = len(hometeam)
 
 
 
