10dd338b31c633458efa234e41d34f901bf8754a
JBBart <schwarzbartj@gmail.com>
2017-09-11 18:42:29 +0200

    basic Flask application

--- app.py
+++ app.py
@@ -1,13 +1,31 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 
 
 app = Flask(__name__) # determine the route path
 
 # @ signifies a decorator - way to wrap a function and modifying its behavior
-@app.route('/') # Homepage of the website
+@app.route("/") # Homepage of the website
 def index():
-    return '<h2>This is the homepage</h2>'
+    return "Spread Bets"
+
+
+@app.route("/profile/<username>") # variables are in '< >'
+def profile(username):
+    return render_template("profile.html", username=username)
+
+@app.route("/week/<int:week>") # ints have to be announced like this
+def week(week):
+    return "This is week {}".format(week)
+
+
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True) # start the server
